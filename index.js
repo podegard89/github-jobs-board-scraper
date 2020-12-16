93b2ae4897c4f7fa66acec7a31fe34ad7c6b4599
Pearce Odegard <pearceodegard@gmail.com>
2020-12-16 03:45:38 -0600

    Script scrapes multiple pages

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 const Sheet = require('./sheet');
 const fetch = require('node-fetch');
 
-(async function () {
-    const sheet = new Sheet();
-    await sheet.load();
 
-    const res = await fetch('https://jobs.github.com/positions.json?location=remote');
+
+async function scrapePage(i) {
+    const res = await fetch(`https://jobs.github.com/positions.json?page=${i}&search=code`);
     const json = await res.json();
 
     const rows = json.map(job => {
@@ -18,5 +17,28 @@ const fetch = require('node-fetch');
         }
     })
 
+    return rows;
+}
+
+(async function () {
+
+    let i = 1;
+    let rows = [];
+    while(true) {
+        const newRows = await scrapePage(i);
+        if (newRows.length === 0) break;
+        rows = rows.concat(newRows);
+        i++;
+    }
+
+    const sheet = new Sheet();
+    await sheet.load();
+
     await sheet.addRows(rows);
 })()
+
+
+
+
+    
+
